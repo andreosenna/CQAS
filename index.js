cd0b8a5f789de1e25139d5ff20896b506a8b329e
AndreOSenna <54328014+andreosenna@users.noreply.github.com>
2023-10-22 10:32:16 -0300

    corrigindo index.js ES6

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-import express from 'express'
-import bodyparser from 'body-parser'
+import express from 'express';
+import bodyparser from 'body-parser';
 import Posts from './Models/Posts.js';
-import Path from 'path'
-const app = express() 
-const InstPost = new Posts() 
+import Path from 'path';
+import { fileURLToPath } from 'url'; // Importe fileURLToPath
+const __filename = fileURLToPath(import.meta.url); // Obtenha __filename usando import.meta.url
+const __dirname = Path.dirname(__filename); // Obtenha __dirname do __filename
 
-app.use(express.static(Path.join(__dirname,"public")))
-const PORT = 3500
-app.listen(PORT,()=>{
-    console.log("servidor iniciado na porta",PORT);
-})
+const app = express();
+const InstPost = new Posts();
 
-app.get("/all",(rec,res)=>{
-res.json(JSON.stringify(InstPost.getAll()))
-})
+app.use(express.static(Path.join(__dirname, "public")));
+const PORT = 3500;
 
-app.post("/new",bodyparser.json(),(req,res)=>{
-    let nome = req.body.nome
-    let cargo = req.body.cargo
+app.listen(PORT, () => {
+  console.log("servidor iniciado na porta", PORT);
+});
 
-    InstPost.newPost(nome,cargo)
-    
-    res.send("post enviado")
-    })
+app.get("/all", (req, res) => {
+  res.json(JSON.stringify(InstPost.getAll()));
+});
 
+app.post("/new", bodyparser.json(), (req, res) => {
+  let nome = req.body.nome;
+  let cargo = req.body.cargo;
+
+  InstPost.newPost(nome, cargo);
+
+  res.send("post enviado");
+});
